987ab284d8391a8f5e66b2ee24ed6365f67a956a
Marcella Costa <79029735+marcellacg@users.noreply.github.com>
2022-11-01 12:11:26 -0300

    Delete AppCRUD directory

--- AppCRUD/src/main/java/br/com/crudproj/applicationcrud/Repository/RepoProduto.java
+++
@@ -1,9 +0,0 @@
-package br.com.crudproj.applicationcrud.Repository;
-
-import br.com.crudproj.applicationcrud.Models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepoProduto extends JpaRepository<Produto, Long> {
-
-}
-
--- AppCRUD/src/main/java/br/com/crudproj/applicationcrud/Repository/RepoUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.crudproj.applicationcrud.Repository;
-
-import br.com.crudproj.applicationcrud.Models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepoUsuario extends JpaRepository<Usuario, Long> {
-
-}
