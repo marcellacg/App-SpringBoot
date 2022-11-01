0bbaeafc25e12a1e5b492563a91ce9d55ace99ef
Marcella Costa <79029735+marcellacg@users.noreply.github.com>
2022-11-01 01:20:28 -0300

    Add files via upload

---
+++ AppCRUD/src/main/java/br/com/crudproj/AppCRUD/AppCrudApplication.java
@@ -0,0 +1,14 @@
+package br.com.crudproj.AppCRUD;
+
+import br.com.crudproj.AppCRUD.Models.Usuario;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCrudApplication.class, args);
+	}
+
+}
